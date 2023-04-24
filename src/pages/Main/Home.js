dc0c3f45effdae92e688971e7c7d84437cef54b2
mostofa-s-cse <mostofa.s.cse@gmail.com>
2023-04-24 21:49:55 +0600

    03. Impementing basic filter - part 2 - Adding Stock toggle and handling filter UI

--- src/pages/Main/Home.js
+++ src/pages/Main/Home.js
@@ -1,11 +1,15 @@
 import React, { useEffect, useState } from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import ProductCard from "../../components/ProductCard";
-import { toggleBrand } from "../../redux/actions/filterActions";
+import { toggleBrand, toggleStock } from "../../redux/actions/filterActions";
 
 const Home = () => {
   const [products, setProducts] = useState([]);
   const dispatch = useDispatch();
+  const filters = useSelector((state) => state.filter.filters);
+  // console.log("filters", filters);
+  const { brands, stock } = filters;
+  // console.log(brands, stock);
 
   useEffect(() => {
     fetch("http://localhost:5000/products")
@@ -16,25 +20,31 @@ const Home = () => {
   const activeClass = "text-white  bg-indigo-500 border-white";
 
   return (
-    <div className='max-w-7xl gap-14 mx-auto my-10'>
-      <div className='mb-10 flex justify-end gap-5'>
+    <div className="max-w-7xl gap-14 mx-auto my-10">
+      <div className="mb-10 flex justify-end gap-5">
         <button
-          className={`border px-3 py-2 rounded-full font-semibold ${activeClass} `}
-        >
+          onClick={() => dispatch(toggleStock())}
+          className={`border px-3 py-2 rounded-full font-semibold ${
+            stock ? activeClass : null
+          } `}>
           In Stock
         </button>
-        <button 
-        onClick={()=>dispatch(toggleBrand("amd"))}
-        className={`border px-3 py-2 rounded-full font-semibold`}>
+        <button
+          onClick={() => dispatch(toggleBrand("amd"))}
+          className={`border px-3 py-2 rounded-full font-semibold ${
+            brands.includes("amd") ? activeClass : null
+          }`}>
           AMD
         </button>
-        <button 
-        onClick={()=>dispatch(toggleBrand("intel"))}
-        className={`border px-3 py-2 rounded-full font-semibold`}>
+        <button
+          onClick={() => dispatch(toggleBrand("intel"))}
+          className={`border px-3 py-2 rounded-full font-semibold ${
+            brands.includes("intel") ? activeClass : null
+          }`}>
           Intel
         </button>
       </div>
-      <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-14'>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-14">
         {products.map((product) => (
           <ProductCard key={product.model} product={product} />
         ))}
